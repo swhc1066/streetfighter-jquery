24471bed3951f85f53c7372a930407d8f9625e8d
Sean Craig <fwcycling@gmail.com>
2015-05-23 17:45:13 -0500

    initial commit

---
+++ js/app.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+	$('.ryu').mouseenter(function() {
+		$('.ryu-still').hide();
+		$('.ryu-ready').show();
+	})
+
+	.mouseleave(function() {
+		$('.ryu-ready').hide();
+		$('.ryu-still').show();
+	})
+
+	.mousedown(function() {
+		playHadouken();
+		$('.ryu-still').hide();
+		$('.ryu-ready').hide();
+		$('.ryu-throwing').show();
+		$('.hadouken').finish().show().animate(
+			{'left': '1020px'},
+			500,
+			function() {
+				$(this).hide();
+				$(this).css('left', '520px');
+			}
+		);
+	})
+
+	.mouseup(function() {
+		$('.ryu-ready').show();
+		$('.ryu-throwing').hide();
+		// ryu goes back to ready position
+	});
+
+	$(document).keydown(function(e) {                 
+           
+               if (e.keyCode == 88) {
+               $('.ryu-still').hide();
+               $('.ryu-ready').hide();
+               $('.ryu-cool').show();
+               }
+           })  
+
+          .keyup(function(e) {
+               if (e.keyCode == 88) {
+               $('.ryu-cool').hide();
+               $('.ryu-still').show();
+               }
+           })
+});
+
+function playHadouken () {
+	$('#hadouken-sound')[0].volume = 0.5;
+	$('#hadouken-sound')[0].load();
+	$('#hadouken-sound')[0].play();
+}
